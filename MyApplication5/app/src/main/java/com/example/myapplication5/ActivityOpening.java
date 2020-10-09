ad53070363336ab0fb4b104850fcafa285e06531
linhlevn2 <linhlevn2@gmail.com>
2020-10-09 00:35:02 -0400

    Adding music while on the opening screen
    add a song to raw resource & add song.stop() into the TimerTask task to make it also run for 5s

--- MyApplication5/app/src/main/java/com/example/myapplication5/ActivityOpening.java
+++ MyApplication5/app/src/main/java/com/example/myapplication5/ActivityOpening.java
@@ -3,20 +3,25 @@ package com.example.myapplication5;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 
 import java.util.Timer;
 import java.util.TimerTask;
 
 public class ActivityOpening extends AppCompatActivity {
-
+    MediaPlayer song;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_opening);
+        song = MediaPlayer.create(this, R.raw.song);
+        song.start();
+        
         TimerTask task = new TimerTask() {
             @Override
             public void run() {
+                song.stop();
                 finish();
                 startActivity(new Intent(ActivityOpening.this, MainActivity.class));
             }
