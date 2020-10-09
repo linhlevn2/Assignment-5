5a6c8a0193952c4561508bbd263fad27e11b7d1d
linhlevn2 <linhlevn2@gmail.com>
2020-10-08 23:46:28 -0400

    Update activity_opening.java
    add a Timer and startActivity to open the MainActivity after 5s

--- MyApplication5/app/src/main/java/com/example/myapplication5/activity_opening.java
+++ MyApplication5/app/src/main/java/com/example/myapplication5/activity_opening.java
@@ -2,7 +2,11 @@ package com.example.myapplication5;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class activity_opening extends AppCompatActivity {
 
@@ -10,5 +14,14 @@ public class activity_opening extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_opening);
+        TimerTask task = new TimerTask() {
+            @Override
+            public void run() {
+                finish();
+                startActivity(new Intent(activity_opening.this, MainActivity.class));
+            }
+        };
+        Timer opening = new Timer();
+        opening.schedule(task,5000);
     }
 }
